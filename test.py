65edd46eefb072b96c4534c544d655fff24733bd
Jarvis-00 <104796883+Jarvis-070@users.noreply.github.com>
2024-12-27 00:11:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ print('\033[0;97m [√] \033[92mchecking for update')
 os.system('git pull --quiet 2>/dev/null')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    print(system supported)
+    print('system supported')
     import test
 elif bit == '32bit':
     print("\033[1;90m [\033[1;91m system not supported \033[1;90m]");exit()
